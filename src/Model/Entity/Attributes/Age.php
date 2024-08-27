bf8a8e8f1804fe768d4d6dd9c39d81b7b489e855
Jan Galek <admin@gcore.cz>
2024-08-27 08:25:38 +0200

    Added attribute

---
+++ src/Model/Entity/Attributes/Age.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace EFabrica\Model\Entity\Attributes;
+
+use EFabrica\Model\Entity\Attribute;
+
+class Age implements Attribute
+{
+    public function __construct(int $age)
+    {
+
+    }
+    public function setName(string $name): void
+    {
+        // TODO: Implement setName() method.
+    }
+
+    public function getName(): string
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function setValue(mixed $value): mixed
+    {
+        // TODO: Implement setValue() method.
+    }
+
+    public function getValue(): mixed
+    {
+        // TODO: Implement getValue() method.
+    }
+}
