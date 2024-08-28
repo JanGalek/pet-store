d4b3615104bea4a439b386ab6f3723e40f6f4c12
Jan Galek <admin@gcore.cz>
2024-08-28 19:38:04 +0200

    fixup! Added api

--- src/Model/Entity/Category.php
+++ src/Model/Entity/Category.php
@@ -2,11 +2,21 @@
 
 namespace EFabrica\Model\Entity;
 
+use EFabrica\Services\Uuid\Generator;
+use Ramsey\Uuid\UuidInterface;
+
 class Category
 {
+    protected UuidInterface $id;
     public function __construct(
         private string $name
     ) {
+        $this->id = Generator::create();
+    }
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
     }
 
     public function getName(): string
